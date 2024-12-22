08ad630eac819adffe0d5a21bffc172f7d4527f2
soubhagyakuanr <147742784+soubhagyakuanr@users.noreply.github.com>
2024-12-22 23:40:27 +0530

    script file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 let todoList=[];
+function addTodo(){
+  let inputelement=document.querySelector('#todo_input');
+  let dateelement=document.querySelector('#todo_date');
+
+  let todoitem=inputelement.value;
+  let tododate=dateelement.value;
+  console.log(todoitem);
+  todoList.push({item:todoitem,duedate:tododate});
+  inputelement.value='';
+  dateelement.value='';
+  displayitems();
+}
